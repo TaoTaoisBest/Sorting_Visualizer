191a0f7bca9ab9650149f9f8a17eba87e03d13ee
TaoTaoisbest <popopig881209@gmail.com>
2021-12-02 08:23:55 +0800

    attemp to bubble sort

--- client/src/components/sortScreen/bubble_sort/bubble_sort.js
+++ client/src/components/sortScreen/bubble_sort/bubble_sort.js
@@ -2,28 +2,39 @@ import React, { Component } from 'react';
 import '../bars.css';
 import '../bars_container.css';
 
-
 class BubbleSort extends Component {
     constructor(props) {
         super(props);
-
+        this.state={
+            arr : this.props.contentArray.map((value) => 
+                <li className='bars' id={value} key={value} style={{width : value}}>
+                    {value}
+                </li>
+            ),
+        };
         console.log(this.props.contentArray);
     }
 
-    swap(arr, val1, val2) {
-        var temp = arr[val1];
-        arr[val1] = arr[val2];
-        arr[val2] = temp;
+    sleep = (milliseconds) => {
+        return new Promise(resolve => setTimeout(resolve, milliseconds))
     }
 
-    bubble_sort(arr) {
-        var n = arr.length;
+    swap(val1, val2) {
+        let arr = [...this.state.arr];
+        let element1 = {...arr[val1]};
+        let element2 = {...arr[val2]};
+        arr[val1]=element2;
+        arr[val2]=element1;
+        this.setState({arr});
+    }
+
+    bubble_sort() {
+        var n = this.state.arr.length;
         var i, j;
-        
-        for(i = 0; i < n-1; i++) {
-            for(j = 0; j < n-i-1; j++) {
-                if(arr[j] > arr[j+1]) {
-                    this.swap(arr, j, j+1);
+        for(i = 1; i < n; i++) {
+            for(j = 0; j < n-i; j++) {
+                if(parseInt(this.state.arr[j].key) > parseInt(this.state.arr[j+1].key) ) {
+                    this.sleep(1000).then(this.swap(j, j+1));
                 }
             }
         }
@@ -31,17 +42,11 @@ class BubbleSort extends Component {
 
     render(){
         console.log("IN bubble_sort.js");
-
-        var videos_bars = this.props.contentArray.map((value) => 
-            <li className='bars' id={value} key={value} style={{width : value}}>
-                {value}
-            </li>
-        );
-
         return(
             <div>
                 <ul className='bars_container'>
-                    {videos_bars}
+                    {this.state.arr}
+                    {this.bubble_sort()}
                 </ul>
             </div>
         )
